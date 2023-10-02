5095710670190b27e85be95d204e55f25f684d53
Nicholas Hellmers <nihe1661@colorado.edu>
2023-10-02 00:00:37 -0600

    added

--- app/app/components/About.tsx
+++ app/app/components/About.tsx
@@ -9,7 +9,7 @@ export default function About() {
             I am a software engineer with a passion for learning and creating new exciting things. I have experience with a wide range of technologies and languages, and I am always looking to learn more.
         </p>
         <p>
-            I am currently working in mastering the concepts of the uses of Golang on the web.
+            I am currently working in mastering the concepts of the uses of Go on the web.
         </p>
     </div>
   )
