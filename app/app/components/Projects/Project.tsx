601e76ace9630bf4f48a0e247117ed765d35b9ea
Nicholas Hellmers <nihe1661@colorado.edu>
2024-01-17 22:46:35 -0700

    changed project styling

--- app/app/components/Projects/Project.tsx
+++ app/app/components/Projects/Project.tsx
@@ -7,9 +7,9 @@ type project = {
 
 export default function Project(props: project) {
   return (
-    <a className="m-auto border rounded-lg overflow-hidden" href={props.project_url} target="_blank">
-        <div className="leading-loose">
-                <img src={props.img_url} alt={props.name}/>
+    <a className="m-auto h-[100%] border rounded-lg overflow-hidden" href={props.project_url} target="_blank">
+        <div className="leading-loose max-w-[25rem]">
+                <img className="object-cover h-52" src={props.img_url} alt={props.name}/>
                 <div className="py-3 px-5">
                   <h1>{props.name}</h1>
                   <p className="text-sm font-light text-gray-300">{props.desc}</p>
