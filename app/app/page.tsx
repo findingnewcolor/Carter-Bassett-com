920bb6e00da3681fcb5725b2c5a9b15833201f94
Nicholas Hellmers <nihe1661@colorado.edu>
2024-04-24 16:38:30 -0600

    fixed img gap

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className='py-12 w-screen flex flex-col justify-center items-center gap-12'>
+      <div className='py-8 w-screen flex flex-col justify-center items-center gap-12'>
         <Image src='/profile.jpeg' alt='Profile Picture' width={200} height={200} className='rounded-full' />
-        <div className='grid'>
+        <div className='grid gap-12'>
           <h1 className='text-center text-3xl'>Nicholas Hellmers Davalos</h1>
           <h3 className='text-center text-2xl'>Software Engineer</h3>
         </div>
