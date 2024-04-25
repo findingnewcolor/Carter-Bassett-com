b96cfd7b30bb8e61285176b2323780ee47491630
Nicholas Hellmers <nihe1661@colorado.edu>
2024-04-25 17:34:22 -0600

    fix warning <img>

--- app/app/components/Projects/Project.tsx
+++ app/app/components/Projects/Project.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 type project = {
     name:         string,
     desc:         string,
@@ -9,7 +11,7 @@ export default function Project(props: project) {
   return (
     <a className="m-auto h-[100%] border rounded-lg overflow-hidden" href={props.project_url} target="_blank">
         <div className="leading-loose max-w-[25rem]">
-                <img className="object-cover h-52 w-[100%]" src={props.img_url} alt={props.name}/>
+                <Image className="object-cover h-52 w-[100%]" src={props.img_url} alt={props.name}/>
                 <div className="py-3 px-5">
                   <h1>{props.name}</h1>
                   <p className="text-sm font-light text-gray-300">{props.desc}</p>
