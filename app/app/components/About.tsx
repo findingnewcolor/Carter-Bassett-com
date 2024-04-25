8309a3e2997301afd1cc10a53c90cd7096f0befb
Nicholas Hellmers <nihe1661@colorado.edu>
2024-04-25 17:41:22 -0600

    fixes

--- app/app/components/About.tsx
+++ app/app/components/About.tsx
@@ -6,8 +6,8 @@ export default function About() {
             About Me
         </h2>
         <div className="flex flex-col justify-center items-center gap-8">
-          <p>Hi! I'm a dedicated software engineer driven by a relentless curiosity and a deep passion for technology. My journey in the tech industry has allowed me to explore and gain expertise in a diverse array of programming languages and technologies. My approach is always centered around crafting innovative solutions and pushing the boundaries of what's possible in software development.</p>
-          <p>Currently, I am delving deep into the world of Go, focusing on leveraging its power to build robust, scalable web applications. My goal is to master Go's unique capabilities in web development to enhance user experiences and optimize backend performance.</p>
+          <p>Hi! I&apos;m a dedicated software engineer driven by a relentless curiosity and a deep passion for technology. My journey in the tech industry has allowed me to explore and gain expertise in a diverse array of programming languages and technologies. My approach is always centered around crafting innovative solutions and pushing the boundaries of what&apos;s possible in software development.</p>
+          <p>Currently, I am delving deep into the world of Go, focusing on leveraging its power to build robust, scalable web applications. My goal is to master Go&apos;s unique capabilities in web development to enhance user experiences and optimize backend performance.</p>
         </div>
     </div>
   )
